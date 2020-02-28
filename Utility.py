81ac4cbcbc7ed4638685cf34753c94718cb37467
Ettore <ettorecelozzi@gmail.com>
2020-02-28 19:54:06 +0100

    bug fix

--- Utility.py
+++ Utility.py
@@ -1,68 +1,45 @@
 import numpy as np
 
-def check_states(states, qTables):
+
+def check_states(states):
     """
     Verify if the new states belong to the discrete grid
     :param states: float states
-    :param qTables: QTable of the agent. Two for decentralized
     :return: states in the discrete grid
     """
     positions = np.round(list(np.linspace(-1, 1, 100)), decimals=2)
     velocities = np.round(list(np.linspace(-3, 3, 50)), decimals=2)
-    position_space = 1/50
-    velocity_space = 3/25
-    position_index = np.abs(int(np.round(states[0] / position_space,decimals=0)))
+    position_space = 1 / 50
+    velocity_space = 3 / 25
+    position_index = np.abs(int(np.round(states[0] / position_space, decimals=0)))
     velocity_index = np.abs(int(np.round(states[1] / velocity_space, decimals=0)))
+
     if states[0] > 0:
         position_index += 50
     if states[1] > 0:
         velocity_index += 25
 
     if position_index == 100:
-        possible_positions = [positions[position_index - 1],positions[position_index - 2]]
+        possible_positions = positions[position_index - 2:]
     elif position_index == 0:
-        possible_positions = [positions[position_index], positions[position_index + 1]]
+        possible_positions = positions[:position_index + 2]
+    elif position_index == 99:
+        possible_positions = positions[position_index - 1:position_index + 1]
     else:
-        if position_index == 99:
-            possible_positions = [positions[position_index - 1], positions[position_index]]
-        else:
-            possible_positions = [positions[position_index - 1], positions[position_index], positions[position_index + 1]]
+        possible_positions = positions[position_index - 1:position_index + 2]
 
     if velocity_index == 50:
-        possible_velocities = [velocities[velocity_index - 1],velocities[velocity_index -2]]
+        possible_velocities = velocities[velocity_index - 2:]
     elif velocity_index == 0:
-        possible_velocities = [velocities[velocity_index], velocities[velocity_index + 1]]
+        possible_velocities = velocities[:velocity_index + 2]
+    elif velocity_index == 49:
+        possible_velocities = velocities[velocity_index - 1:velocity_index + 1]
     else:
-        if velocity_index == 44:
-            possible_velocities = [velocities[velocity_index - 1], velocities[velocity_index]]
-        else:
-            possible_velocities = [velocities[velocity_index - 1], velocities[velocity_index],
-                                   velocities[velocity_index + 1]]
-    new_states = (min(possible_positions, key=lambda x:abs(x-states[0])),min(possible_velocities, key=lambda x:abs(x-states[1])))
+        possible_velocities = velocities[velocity_index - 1:velocity_index + 2]
+
+    new_states = (min(possible_positions, key=lambda x: abs(x - states[0])),
+                  min(possible_velocities, key=lambda x: abs(x - states[1])))
     return new_states
-
-    discrete_states = -1
-    discrete = False
-    for q in range(len(qTables)):
-        keys = list(qTables[q].keys())
-        if states not in keys:
-            # discrete_states = min(keys, key=lambda x: euclidean(x, states))
-            discrete_states = min(keys, key=lambda x: tuple_distance(x, states))
-            discrete = True
-    return discrete_states if discrete else states
-
-
-def tuple_distance(tuple1, tuple2):
-    """
-    Faster distances. Squared distances to speed up the calculation
-    :param tuple1: first tuple
-    :param tuple2: second tuple
-    :return: Squared distances.
-    """
-    distance = 0
-    for t1, t2 in zip(tuple1, tuple2):
-        distance += (t1 - t2) ** 2
-    return distance
 
 
 def check_actions(new_actions, actions):
